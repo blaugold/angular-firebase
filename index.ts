6a5279ec83155c5e2018d00f2d6aa0698c47c031
Gabriel Terwesten <gabriel@terwesten.net>
2016-11-04 16:54:24 +0100

    make angular-logger peerDep and export all public objects in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
+export * from './src/data-snapshot-observable'
+export * from './src/firebase.module'
+export * from './src/firebase-app.service'
 export * from './src/firebase-auth.service'
 export * from './src/firebase-database.service'
-export * from './src/firebase.module'
 export * from './src/reexports'
