cc2f79c1461926de3c4b5f75a65f1650eb82432c
Gabriel Terwesten <gabriel@terwesten.net>
2017-03-24 09:13:13 +0100

    build(): bundle package with rollup

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+/**
+ * Created by gabrielterwesten on 24/03/2017.
+ */
+const pkg = require('./package.json');
+const external = [
+  ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.peerDependencies)
+];
+
+export default {
+  entry: './release/src/index.js',
+  external,
+  targets: [
+    {
+      dest: getLocalDest(pkg.main),
+      format: 'umd',
+      moduleName: pkg.name,
+      sourceMap: true
+    },
+    {
+      dest: getLocalDest(pkg.module),
+      format: 'es',
+      sourceMap: true
+    }
+  ]
+}
+
+function getLocalDest(releaseDest) {
+  return `./release/${releaseDest}`
+}
