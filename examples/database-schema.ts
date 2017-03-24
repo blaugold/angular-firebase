a6f2a885f95aaa3d4ebb07d2eab4cd207b8688fb
Gabriel Terwesten <gabriel@terwesten.net>
2017-03-24 09:12:39 +0100

    docs(): expand example

--- examples/database-schema.ts
+++ examples/database-schema.ts
@@ -1,10 +1,15 @@
-import { FirebaseDatabase } from '../src'
+import { FirebaseDatabase } from '@blaugold/angular-firebase'
+import 'rxjs/add/operator/map'
+
+/**
+ * This example demonstrates the type checking ability when the database is used with a schema.
+ * Open this file with an ide like VS Code or WebStorm and you should see error highlighting.
+ */
 
 interface DBSchema {
   todoLists: {
     [listId: string]: {
       name: string
-      createdBy: string
       todoItems: {
         [itemId: string]: {
           title: string
@@ -13,16 +18,19 @@ interface DBSchema {
       }
     }
   }
-  users: {
-    [userId: string]: {
-      name: string
-      email: string
-      birthday: string
-    }
-  }
 }
 
 let db: FirebaseDatabase<DBSchema>
 
+// Should compile.
 db.ref().child('todoLists').child('1').child('todoItems')
-  .onValue().values().map(x => x.map(xs => xs.checked))
+
+db.ref().child('todoLists').child('1').child('name').onValue().val()
+  .map((name: string /* Compiles since `name`has type string. */) => {})
+  
+
+// Should not compile.
+db.ref().child('todoLists').child('1').child('todoItemz') // todoItemz should be todoItems.
+  
+db.ref().child('todoLists').child('1').child('name').onValue().val()
+  .map((name: number /* Fails since `name` has type string. */) => {})
